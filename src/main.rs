f60f975bbfa7dc071b81f781e76b9a5df9952516
vkazbanov <vkazbanov@telematika.com>
2024-05-05 18:18:33 +0200

    Initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,81 @@
-// #![deny(unsafe_code)]
-#![no_std]
+#![allow(clippy::empty_loop)]
 #![no_main]
+#![no_std]
 
 use defmt_rtt as _;
 use panic_probe as _;
 
-use cortex_m::singleton;
-use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*};
+#[rtic::app(device = stm32f1xx_hal::pac)]
+mod app {
+    use core::sync::atomic::{AtomicU32, Ordering};
+    use stm32f1xx_hal::{
+        gpio::{gpioa::PA0, Edge, ExtiPin, Input},
+        prelude::*,
+    };
+    use stm32f1xx_hal::device::TIM1;
+    use stm32f1xx_hal::gpio::{Floating, IOPinSpeed, OutputSpeed};
+    use stm32f1xx_hal::timer::PwmChannel;
 
-use stm32f1xx_hal::adc::{Adc, SampleTime};
-use stm32f1xx_hal::serial::{Config, Serial};
+    static TICKS: AtomicU32 = AtomicU32::new(0);
 
-#[entry]
-fn main() -> ! {
-    // Get access to the core peripherals from the cortex-m crate
-    let core = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let device = pac::Peripherals::take().unwrap();
-
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
-    let mut flash = device.FLASH.constrain();
-    let rcc = device.RCC.constrain();
-
-    defmt::info!("Loading");
-
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
-    let clocks = rcc.cfgr
-        .sysclk(72.MHz())
-        .hclk(72.MHz())
-        .pclk1(36.MHz())
-        .pclk2(72.MHz())
-        .freeze(&mut flash.acr);
-
-    let mut afio = device.AFIO.constrain();
-
-    /*// Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split();
-
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
-
-    // Wait for the timer to trigger an update and change the state of the LED
-    loop {
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
-    }*/
-
-    // Setup pins
-    let mut gpioa = device.GPIOA.split();
-    let mut gpiob = device.GPIOB.split();
-
-    let dma = device.DMA1.split();
-    let mut dma_ch1 = dma.1;
-    let mut dma_ch2 = dma.2;
-
-    let mut adc = Adc::adc1(device.ADC1, clocks);
-    adc.set_sample_time(SampleTime::T_1);
-
-    let tx = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
-    let rx = gpiob.pb11;
-    let mut sc = Config::default();
-    sc.baudrate = 256000.bps();
-
-    let mut serial = Serial::new(device.USART3, (tx, rx), &mut afio.mapr, sc, &clocks);
-
-    // Configure pa0 as an analog input
-    let mut adc_ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
-
-    // let mut timer = dp.TIM3.delay_us(&clocks);
-
-    let mut buffer = singleton!(: [u16; 1024] = [0; 1024]).unwrap().as_mut_slice();
-    let mut sending = singleton!(: [u16; 1024] = [0; 1024]).unwrap().as_mut_slice();
-
-    defmt::info!("vref");
-
-    let vref = adc.read_vref();
-
-    defmt::info!("measure");
-
-    loop {
-        let val: u16 = nb::block!(adc.read(&mut adc_ch0)).unwrap();
-        serial.bwrite_all(val.to_ne_bytes().as_slice()).unwrap();
+    #[shared]
+    struct Shared {
     }
 
-    let (mut s_tx, _) = serial.split();
+    #[local]
+    struct Local {
+        button: PA0<Input<Floating>>,
+        ch0: PwmChannel<TIM1, 0>,
+    }
 
-    loop {
-        let v: u16 = nb::block!(adc.read(&mut adc_ch0)).unwrap();
-        if v > 500 / vref {
-            let adc_dma = adc.with_dma(adc_ch0, dma_ch1);
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local) {
+        let mut afio = ctx.device.AFIO.constrain();
+        let mut flash = ctx.device.FLASH.constrain();
+        let mut exti = ctx.device.EXTI;
+        let rcc = ctx.device.RCC.constrain();
+        let clocks = rcc.cfgr
+            .sysclk(72.MHz())
+            .use_hse(8.MHz())
+            .pclk1(36.MHz())
+            .pclk2(72.MHz())
+            .freeze(&mut flash.acr);
 
-            let (buf, adc_dma) = adc_dma.read(buffer).wait();
+        let mut gpioa = ctx.device.GPIOA.split();
+        let mut led = gpioa.pa8.into_alternate_push_pull(&mut gpioa.crh);
+        led.set_speed(&mut gpioa.crh, IOPinSpeed::Mhz50);
+        let etr = gpioa.pa12.into_alternate_push_pull(&mut gpioa.crh);
 
-            for c in &mut *buf {
-                *c = v;
-            }
+        let timer = ctx.device.TIM1;
+        // timer.counter(&clocks).set_master_mode(MMS_A::ComparePulse)
+        let timer = timer.pwm_hz(led, &mut afio.mapr, (256 * 50 / 2).Hz(), &clocks);
+        let max = timer.get_max_duty();
+        let mut ch0 = timer.split();
+        ch0.enable();
+        ch0.set_duty(max / 2);
 
-            let dma_tx = s_tx.with_dma(dma_ch2);
+        let mut button = gpioa.pa0.into_floating_input(&mut gpioa.crl);
+        button.make_interrupt_source(&mut afio);
+        button.enable_interrupt(&mut exti);
+        button.trigger_on_edge(&mut exti, Edge::Rising);
+        defmt::println!("init");
 
-            let (buf, tx_dma) = dma_tx.write(bytemuck::cast_slice_mut::<u16, u8>(buf)).wait();
+        (Shared { }, Local { button, ch0 })
+    }
 
-            let (tx, tch) = tx_dma.release();
-
-            // Consumes the AdcDma struct, restores adc configuration to previous state and returns the
-            // Adc struct in normal mode.
-            let (a, ach, dch) = adc_dma.split();
-
-            adc = a;
-            adc_ch0 = ach;
-            dma_ch1 = dch;
-            dma_ch2 = tch;
-            buffer = bytemuck::cast_slice_mut::<u8, u16>(buf);
-            s_tx = tx;
-            // timer.delay_us(297)
+    #[idle]
+    fn idle(_ctx: idle::Context) -> ! {
+        loop {
+            TICKS.fetch_add(1, Ordering::Relaxed);
         }
+    }
+
+    #[task(binds = EXTI0, local = [button, ch0], priority = 2)]
+    fn pps_tick(ctx: pps_tick::Context) {
+        ctx.local.button.clear_interrupt_pending_bit();
+        let timer: TIM1 = unsafe { core::mem::transmute(()) };
+        timer.cr1.write(|w| w.cen().clear_bit());
+        timer.ccr1().write(|w| w.ccr().variant(0));
+        ctx.local.ch0.set_duty(ctx.local.ch0.get_duty());
     }
 }
