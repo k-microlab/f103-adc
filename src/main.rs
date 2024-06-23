d18800f8558212a173a8aa04d93ee3ae34ee2598
vkazbanov <vkazbanov@telematika.com>
2024-06-23 19:35:32 +0200

    Working timer irq

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,20 @@ use panic_probe as _;
 #[rtic::app(device = stm32f1xx_hal::pac)]
 mod app {
     use core::sync::atomic::{AtomicU32, Ordering};
+    use cortex_m::peripheral::NVIC;
+    use cortex_m::singleton;
+    use nb::block;
     use stm32f1xx_hal::{
         gpio::{gpioa::PA0, Edge, ExtiPin, Input},
         prelude::*,
     };
+    use stm32f1xx_hal::adc::{Adc, SampleTime};
     use stm32f1xx_hal::device::TIM1;
-    use stm32f1xx_hal::gpio::{Floating, IOPinSpeed, OutputSpeed};
-    use stm32f1xx_hal::timer::PwmChannel;
+    use stm32f1xx_hal::gpio::{Alternate, Analog, Floating, IOPinSpeed, Output, OutputSpeed, PA1, PA10, PA15, PA9, PC13};
+    use stm32f1xx_hal::pac::{Interrupt, USART1};
+    use stm32f1xx_hal::serial::{Config, Serial};
+    use stm32f1xx_hal::stm32::ADC1;
+    use stm32f1xx_hal::timer::{CounterHz, Event};
 
     static TICKS: AtomicU32 = AtomicU32::new(0);
 
@@ -24,8 +31,13 @@ mod app {
 
     #[local]
     struct Local {
-        button: PA0<Input<Floating>>,
-        ch0: PwmChannel<TIM1, 0>,
+        pps: PA15<Input<Floating>>,
+        led: PC13<Output>,
+        adc: Adc<ADC1>,
+        adc_ch0: PA0<Analog>,
+        adc_ch1: PA1<Analog>,
+        serial: Serial<USART1, (PA9<Alternate>, PA10)>,
+        timer: CounterHz<TIM1>
     }
 
     #[init]
@@ -41,26 +53,48 @@ mod app {
             .pclk2(72.MHz())
             .freeze(&mut flash.acr);
 
+        unsafe {
+            NVIC::unmask(Interrupt::TIM1_CC);
+            NVIC::unmask(Interrupt::TIM1_UP);
+            NVIC::unmask(Interrupt::EXTI15_10);
+        }
+
         let mut gpioa = ctx.device.GPIOA.split();
-        let mut led = gpioa.pa8.into_alternate_push_pull(&mut gpioa.crh);
-        led.set_speed(&mut gpioa.crh, IOPinSpeed::Mhz50);
-        let etr = gpioa.pa12.into_alternate_push_pull(&mut gpioa.crh);
+        let mut gpiob = ctx.device.GPIOB.split();
+        let mut gpioc = ctx.device.GPIOC.split();
 
-        let timer = ctx.device.TIM1;
-        // timer.counter(&clocks).set_master_mode(MMS_A::ComparePulse)
-        let timer = timer.pwm_hz(led, &mut afio.mapr, (256 * 50 / 2).Hz(), &clocks);
-        let max = timer.get_max_duty();
-        let mut ch0 = timer.split();
-        ch0.enable();
-        ch0.set_duty(max / 2);
+        let mut timer = ctx.device.TIM1.counter_hz(&clocks);
+        timer.listen(Event::Update);
+        timer.start(12800.Hz()).unwrap();
 
-        let mut button = gpioa.pa0.into_floating_input(&mut gpioa.crl);
-        button.make_interrupt_source(&mut afio);
-        button.enable_interrupt(&mut exti);
-        button.trigger_on_edge(&mut exti, Edge::Rising);
+        let (pa15, pb3, pb4) = afio.mapr.disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
+
+        let mut adc = Adc::adc1(ctx.device.ADC1, clocks);
+        adc.set_sample_time(SampleTime::T_1);
+
+        let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        led.set_speed(&mut gpioc.crh, IOPinSpeed::Mhz50);
+
+        let mut pps = pa15.into_floating_input(&mut gpioa.crh);
+        pps.make_interrupt_source(&mut afio);
+        pps.enable_interrupt(&mut exti);
+        pps.trigger_on_edge(&mut exti, Edge::RisingFalling);
         defmt::println!("init");
 
-        (Shared { }, Local { button, ch0 })
+        let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+        let rx = gpioa.pa10;
+        let mut sc = Config::default();
+        sc.baudrate = 1024000.bps();
+
+        let mut serial = Serial::new(ctx.device.USART1, (tx, rx), &mut afio.mapr, sc, &clocks);
+
+        // Configure pa0 as an analog input
+        let mut adc_ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
+        let mut adc_ch1 = gpioa.pa1.into_analog(&mut gpioa.crl);
+
+        defmt::info!("init done");
+
+        (Shared { }, Local { pps, led, serial, adc, adc_ch0, adc_ch1, timer })
     }
 
     #[idle]
@@ -70,12 +104,31 @@ mod app {
         }
     }
 
-    #[task(binds = EXTI0, local = [button, ch0], priority = 2)]
+    #[task(binds = TIM1_UP, local = [serial, adc, adc_ch0, adc_ch1, timer])]
+    fn timer(ctx: timer::Context) {
+        let serial = ctx.local.serial;
+        let adc = ctx.local.adc;
+        let adc_ch0 = ctx.local.adc_ch0;
+        let adc_ch1 = ctx.local.adc_ch1;
+        let timer = ctx.local.timer;
+        block!(timer.wait());
+
+        let v0: u16 = block!(adc.read(adc_ch0)).unwrap();
+        let v0 = v0 as i32 * 3300 / 4096 * 5000 / 3000;
+        let v1: u16 = block!(adc.read(adc_ch1)).unwrap();
+        let v1 = v1 as i32 * 3300 / 4096 * 1745 / 1000;
+        let v2: i32 = v0 - v1;
+        serial.bwrite_all((v0 as i16).to_ne_bytes().as_slice()).unwrap();
+        serial.bwrite_all((-(v1 as i16)).to_ne_bytes().as_slice()).unwrap();
+        serial.bwrite_all((v2 as i16).to_ne_bytes().as_slice()).unwrap();
+    }
+
+    #[task(binds = EXTI15_10, local = [pps, led], priority = 2)]
     fn pps_tick(ctx: pps_tick::Context) {
-        ctx.local.button.clear_interrupt_pending_bit();
         let timer: TIM1 = unsafe { core::mem::transmute(()) };
         timer.cr1.write(|w| w.cen().clear_bit());
         timer.ccr1().write(|w| w.ccr().variant(0));
-        ctx.local.ch0.set_duty(ctx.local.ch0.get_duty());
+        ctx.local.pps.clear_interrupt_pending_bit();
+        ctx.local.led.toggle();
     }
 }
